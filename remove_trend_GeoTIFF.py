01127cf5563fdcb71ea3504d059aacd2fb39dfb4
c-mancuso <59205665+c-mancuso@users.noreply.github.com>
2021-03-13 14:41:43 -0500

    Update remove_trend_GeoTIFF.py

--- remove_trend_GeoTIFF.py
+++ remove_trend_GeoTIFF.py
@@ -3,9 +3,9 @@ Remove regional trend from potential field data using least squares
 fitting of 3D polynomial plane. For example, removing regional trend
 from magnetic data. Uses GDAL package.
 
-Written in Python 3.8.5
+Written in Python 3.8
 Christopher Mancuso
-Last updated March 13 2021
+Last updated March 2020
 IN:
 	GeoTIFF image (works best if square dimensions)
 	-filename (see raster_name below)(do not include file extention)
